0ceda17a524af8a29763221c80747e26fcadb207
Franciscovitar <106501087+franciscovitar@users.noreply.github.com>
2026-08-22 18:10:58 -0300

    test: fix player context module import

--- apps/web/lib/player-context.test.ts
+++ apps/web/lib/player-context.test.ts
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
 import test from "node:test";
 
-import { parseCompetitionScopeKey, selectPlayerContext } from "./player-context";
+import { parseCompetitionScopeKey, selectPlayerContext } from "./player-context.ts";
 
 test("parses explicit competition player scopes", () => {
   assert.deepEqual(parseCompetitionScopeKey("competition:ENG_PL:2017/18"), {
